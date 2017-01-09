0cbe74039539bed0f8459f4dd9d08ea3d4ac6df6
Janos Jarecsni <janos.jarecsni@gmail.com>
2017-01-09 05:29:58 +0000

    Added edge describe function

--- ch04/wizards-adventure.lisp
+++ ch04/wizards-adventure.lisp
@@ -6,6 +6,26 @@
     )
   )
 
+(defparameter *edges* 
+  '(
+    (living-room (garden west door) (attic upstairs ladder))
+    (garden (living-room east door))
+    (attic (living-room downstairs ladder))
+    )
+  )
+
 (defun describe-location (location nodes) 
   (cadr (assoc location nodes))
+  )
+
+(defun describe-path (edge)
+  `(there is a ,(caddr edge) going ,(cadr edge) from here.)
+  )
+
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges))))
+  )
+
+(defun describe-paths1 (location edges)
+  (mapcar #'describe-path (cdr (assoc location edges)))
   )
